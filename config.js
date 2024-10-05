4997fe2f73c5f99382a2cf622d8c05343210bf59
Loukson <123784461+Loukson@users.noreply.github.com>
2024-10-05 04:29:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.ANTI_BOT = false;
 global.PM_BLOCKER = false;
 global.CHATBOT = false;
 
-global.SESSION_ID = process.env.SESSION_ID || ""
+global.SESSION_ID = process.env.SESSION_ID || "taira_tech-862b21e4-ead5-4805-87ed-70ca61b00b4f"
 global.Owner = ["2349151864541"]; //like 2347080968564 
 global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
 global.BotName = "♱MAKINO-MD-V2♱♡⃤";
